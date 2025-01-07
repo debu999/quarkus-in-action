bf22cc31f18d5746e8ccd7f196ccf0b9cf21d68e
Debabrata Patnaik <debabrata_patnaik@live.com>
2025-01-07 12:25:57 +0530

    prometheus grafana and jaeger udpate to the app.

--- README.md
+++ README.md
@@ -21,4 +21,21 @@ OpenJDK 64-Bit Server VM Temurin-23.0.1+11 (build 23.0.1+11, mixed mode, sharing
     quarkus.datasource.username=PG_USER
     quarkus.datasource.password=SECRET_PG_CREDS
     ```
+- prometheus.yml
+  - ```yaml
+    global:
+      scrape_interval: 60s
+    remote_write:
+      - url: https://prometheus-prod-xxxxxxxx.grafana.net/api/prom/push
+        basic_auth:
+          username: 1234567
+          password: GRAPHANA_TOKEN_REMOTE_WRITE
+    scrape_configs:
+      - job_name: 'inventory-service'
+        scrape_interval: 10s
+        metrics_path: '/q/metrics'
+        static_configs:
+        - targets:
+          - host.docker.internal:8083
+    ```
 - 
--- inventory-service/src/main/java/org/doogle/inventory/service/GraphQLInventoryService.java
+++ inventory-service/src/main/java/org/doogle/inventory/service/GraphQLInventoryService.java
@@ -1,8 +1,13 @@
 package org.doogle.inventory.service;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
 import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.doogle.inventory.model.Car;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
@@ -10,10 +15,14 @@ import org.eclipse.microprofile.graphql.Query;
 
 @GraphQLApi
 @WithTransaction
+@ApplicationScoped
 public class GraphQLInventoryService {
-//
-//  @Inject
-//  CarInventory inventory;
+  //
+  //  @Inject
+  //  CarInventory inventory;
+
+  @Inject
+  MeterRegistry registry;
 
   @Query
   public Uni<List<Car>> cars() {
@@ -21,6 +30,10 @@ public class GraphQLInventoryService {
   }
 
   @Mutation
+  @Counted(
+      description = "How many cars are registered",
+//      value = "register",
+      extraTags = {"register", "counter", "extra", "annotated"})
   public Uni<Car> register(Car car) {
     return car.<Car>persist().log();
   }
@@ -29,11 +42,12 @@ public class GraphQLInventoryService {
   public Uni<Boolean> remove(String licensePlateNumber) {
     var carUni = Car.findByLicensePlateNumberOptional(licensePlateNumber).log("CAR");
 
-    return carUni.flatMap(car -> {
-      if (car != null) {
-        return car.delete().replaceWith(true);
-      }
-      return Uni.createFrom().item(false);
-    });
+    return carUni.flatMap(
+        car -> {
+          if (car != null) {
+            return car.delete().replaceWith(true);
+          }
+          return Uni.createFrom().item(false);
+        });
   }
 }
